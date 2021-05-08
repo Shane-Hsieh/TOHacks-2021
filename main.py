936f9b81386b537e5a61063ffa8b5f599b7b436d
MylesE <73800016+MylesE@users.noreply.github.com>
2021-05-08 13:16:49 -0400

    Update main.py
    resolved discrepancies

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ client = commands.Bot(command_prefix = "!")
 #commands
 @client.event
 async def on_ready():
-<<<<<<< HEAD
     general_channel = client.get_channel(805633741166477365) #put client ID here
-    #await general_channel.send(hackernews_module.tophn_wrapper(0))
-    #await general_channel.send('!warn @animated')
 
 @client.command()
 async def opgg(ctx):
@@ -26,11 +23,4 @@ async def warn(ctx):
 async def args(ctx, arg1, arg2):
     await bot.say('You sent {} and {}'.format(arg1, arg2))
 
-client.run('ODQwNjIxMTQ2MDgzOTUwNjIy.YJa3cw.xVXFwH8_3rmos_V3qsoZnIl3u1k') #put discord bot token here
-=======
-    general_channel = client.get_channel(ClientID) #put client ID here
-    await general_channel.send('@EGG')
-
-client.run('Token') #put discord bot token here
-
->>>>>>> f933cb3052fa8742af21e2dbbced05932ce13fca
+client.run('') #put discord bot token here
